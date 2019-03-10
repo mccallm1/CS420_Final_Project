62da2727a23295ff18bb5c0de2b7aebbdd2f1409
Miles McCall <mccallm@oregonstate.edu>
2019-03-09 17:00:49 -0800

    add options to Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
-make all: 
+make all:
 	g++ -c main.cpp
 	#g++ -c multiply.cpp
 	g++ -o tsp main.o #multiply.o
+
+make tsp_input:
+	tsp inputs/example.txt
+
+make clean:
+	rm -rf *.o
+	rm -rf tsp
--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@ using namespace std;
 
 int main (int argc, char** argv) {
   // command line args (input file)
-  cout << "You have entered " << argc << " arguments:" << "\n";
-  for (int i = 0; i < argc; ++i)
-        cout << argv[i] << "\n";
+    cout << "You have entered " << argc << " arguments:" << "\n";
+    for (int i = 0; i < argc; ++i)
+          cout << argv[i] << "\n";
 
   // file i/o
-  string line;
-  ifstream myfile ("inputs/example.txt");
-  if (myfile.is_open()) {
-    while ( getline (myfile,line) ) {
-      cout << line << '\n';
+    string line;
+    ifstream myfile ("inputs/example.txt");
+    if (myfile.is_open()) {
+      while ( getline (myfile,line) ) {
+        cout << line << '\n';
+      }
+      myfile.close();
     }
-    myfile.close();
-  }
-  else cout << "Unable to open file";
+    else cout << "Unable to open file";
 
   return 0;
 }
